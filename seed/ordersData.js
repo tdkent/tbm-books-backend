dfbd791c97193cbe5aa654ffa3ef73ea364267a4
Tim Kent <timdekent@gmail.com>
2022-07-17 13:42:13 -0700

    orders tables, working on orders seed data

---
+++ seed/ordersData.js
@@ -0,0 +1,22 @@
+const usersOrders = [
+  {
+    isComplete: false,
+    price: 50.05,
+  },
+  {
+    isComplete: true,
+    price: 43.31,
+  },
+  { isComplete: true, price: 12.09 },
+];
+
+const ordersDetails = [
+  {
+    quantity: 1,
+  },
+  {
+    quantity: 2,
+  },
+];
+
+module.exports = { usersOrders, ordersDetails };
--- seed/seedData.js
+++ seed/seedData.js
@@ -3,22 +3,25 @@ const {
   createBook,
   createAuthor,
   createUser,
-  createOrder,
   createReview,
   getAllAuthorsNoBooks,
   getAllBooksNoReviews,
   getAllUsers,
+  createUserOrder,
+  createOrderDetails,
+  getAllOrders,
 } = require("../db");
 const { authorsData } = require("./authorData");
 const { booksData } = require("./bookData");
 const { reviewsData } = require("./reviewsData");
 const { usersData } = require("./usersData");
+const { usersOrders, ordersDetails } = require("./ordersData");
 
 const createAuthors = async () => {
   try {
     console.log("Adding authors to 'authors' table...");
     const authors = await Promise.all(authorsData.map(createAuthor));
-    console.log("Finished adding authors:", authors);
+    // console.log("Finished adding authors:", authors);
   } catch (err) {
     console.error("An error occurred:", err);
   }
@@ -37,7 +40,7 @@ const createBooks = async () => {
       { authorId: melville.id, ...moby },
     ];
     const books = await Promise.all(data.map(createBook));
-    console.log("Books created:", books);
+    // console.log("Books created:", books);
   } catch (err) {
     console.error("An error occurred:", err);
   }
@@ -47,26 +50,11 @@ const createUsers = async () => {
   try {
     console.log("Adding users to the 'users' table...");
     const users = await Promise.all(usersData.map(createUser));
-    console.log("Users created:", users);
+    // console.log("Users created:", users);
   } catch (err) {
     console.error("An error occurred:", err);
   }
 };
-
-// const createOrders = async () => {
-//   try {
-//     console.log("Adding orders to the 'orders' table...");
-//     const data = [
-//       { quantity: 1, price: 10.99 },
-//       { quantity: 2, price: 25.99 },
-//       { quantity: 1, price: 40.15 },
-//     ];
-//     const orders = await Promise.all(data.map(createOrder));
-//     console.log("Orders created:", orders);
-//   } catch (err) {
-//     console.error("An error occurred:", err);
-//   }
-// };
 
 const createReviews = async () => {
   try {
@@ -103,7 +91,73 @@ const createReviews = async () => {
       },
     ];
     const reviews = await Promise.all(data.map(createReview));
-    console.log("Reviews created:", reviews);
+    // console.log("Reviews created:", reviews);
+  } catch (err) {
+    console.error("An error occurred:", err);
+  }
+};
+
+const createOrders = async () => {
+  try {
+    console.log("Adding orders to users_orders table...");
+    const [tim] = await getAllUsers();
+    const [order1, order2, order3] = usersOrders;
+    const data = [
+      {
+        userId: tim.id,
+        isComplete: order1.isComplete,
+        price: order1.price,
+      },
+      {
+        userId: tim.id,
+        isComplete: order2.isComplete,
+        price: order2.price,
+      },
+      {
+        userId: tim.id,
+        isComplete: order3.isComplete,
+        price: order3.price,
+      },
+    ];
+    const newOrders = await Promise.all(data.map(createUserOrder));
+    console.log("New orders added to users_orders: ", newOrders);
+    const [userOrder1, userOrder2, userOrder3] = await getAllOrders();
+    const [pride, sense, emma, mansfield, moby] = await getAllBooksNoReviews();
+    const [q1, q2] = ordersDetails;
+    const detailsData = [
+      {
+        orderId: userOrder1.id,
+        bookId: pride.id,
+        quantity: q2.quantity,
+      },
+      {
+        orderId: userOrder1.id,
+        bookId: moby.id,
+        quantity: q2.quantity,
+      },
+      {
+        orderId: userOrder2.id,
+        bookId: sense.id,
+        quantity: q1.quantity
+      },
+      {
+        orderId: userOrder3.id,
+        bookId: emma.id,
+        quantity: q2.quantity
+      },
+      {
+        orderId: userOrder3.id,
+        bookId: mansfield.id,
+        quantity: q2.quantity
+      },
+      {
+        orderId: userOrder3.id,
+        bookId: moby.id,
+        quantity: q1.quantity
+      }
+    ];
+    const newOrdersDetails = await Promise.all(detailsData.map(createOrderDetails));
+    console.log("New details added to orders_details table: ", newOrdersDetails);
   } catch (err) {
     console.error("An error occurred:", err);
   }
@@ -113,6 +167,6 @@ module.exports = {
   createBooks,
   createAuthors,
   createUsers,
-  // createOrders,
   createReviews,
+  createOrders,
 };
