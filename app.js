63c64f5e3a18acce52842a2477696384af2f189a
Tim Kent <timdekent@gmail.com>
2022-08-04 20:59:07 -0700

    heroku changes

--- app.js
+++ app.js
@@ -10,6 +10,12 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  next();
+ });
 
 // Stripe Route
 app.post("/create-checkout-session", async (req, res) => {
