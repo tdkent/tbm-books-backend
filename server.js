499357fbf2a6889a9e3849318545e68e2f653f49
Tim Kent <timdekent@gmail.com>
2022-08-06 09:19:05 -0700

    server changes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-const http = require("http");
-const client = require('./client');
+// const http = require("http");
+// const client = require('./client');
 
-const app = require('./app');
+// const app = require('./app');
 
-const PORT = process.env["PORT"] ?? 4000;
-const server = http.createServer(app);
+// const PORT = process.env["PORT"] ?? 4000;
+// const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`CORS-enabled server listening on port ${PORT}`)
-})
+// server.listen(PORT, () => {
+//   console.log(`CORS-enabled server listening on port ${PORT}`)
+// })
 
-client.connect();
+// client.connect();
